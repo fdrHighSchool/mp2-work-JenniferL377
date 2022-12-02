dbca5ac4ecf025bf512424121e839b6fc302ae63
JenniferL377 <114942345+JenniferL377@users.noreply.github.com>
2022-12-02 00:55:54 -0500

    Update Shapes.java

--- 06-shapes/Shapes.java
+++ 06-shapes/Shapes.java
@@ -3,40 +3,55 @@ class Shapes {
         System.out.println("Shapes");
         square(5);
     }
+    //end main
     public static void square(int x) {
         for(int row = 1; row <= x; row++) {
             System.out.println("");
             for (int col = 1; col <= x; col++) {
                 System.out.print("* ");
             }
+            //adds stars to rows
         }
+        //creates rows
     }
+    //end square
     public static void rectangle(int x, int y) {
         for(int row = 1; row <= x; row++) {
             System.out.println("");
             for (int col = 1; col <= y; col++) {
                 System.out.print("* ");
             }
+            //adds stars to rows
         }
+        //creates rows
     }
+    //end rectangle
     public static void righttriangle(int x) {
         for(int row = 1; row <= x; row++) {
             String a = "";
             for (int col = 1; col <= row; col++) {
                 a += "* ";
             }
+            //adds same amount of stars as row #
             System.out.println(a);
         }
+        //creates rows
     }
+    //end righttriangle
     public static void triangle(int x) {
         for(int row = 1; row <= x; row++) {
             System.out.println("");
             for (int space = 1; space <= x-row; space++) {
                 System.out.print(" ");
             }
+            //adds spaces, same # as user input - row #
             for (int col = 1; col <= row; col++) {
                 System.out.print("* ");
             }
+            //adds stars after space, same # as row #
         }
+        //adds rows
     }
+    //end triangle
 }
+//end class
