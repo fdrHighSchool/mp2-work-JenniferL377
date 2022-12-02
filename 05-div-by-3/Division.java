e277028a15622e976bdc41a2e79f747e2dca2623
JenniferL377 <114942345+JenniferL377@users.noreply.github.com>
2022-12-02 00:50:14 -0500

    done

--- 05-div-by-3/Division.java
+++ 05-div-by-3/Division.java
@@ -2,17 +2,22 @@ class Division {
     public static void main(String[] args) {
         System.out.print(divBy3("12123"));
     }
+    //test
     public static boolean divBy3(String a) {
         int b = 0;
         for(int i = 0; i < a.length(); i++) {
             int  c = Integer.parseInt(a.substring(i, i+1));
             b += c;
         }
+        //adds each digit together
         if(b%3==0) {
             return true;
         }
         else {
             return false;
         }
+        //returns true if divisible by three, false otherwise
     }
+    //end divBy3
 }
+//end class
