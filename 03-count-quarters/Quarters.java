897a7d33f14520fb279a4c6f2c9438b4c44b382c
JenniferL377 <114942345+JenniferL377@users.noreply.github.com>
2022-12-02 00:45:18 -0500

    done

--- 03-count-quarters/Quarters.java
+++ 03-count-quarters/Quarters.java
@@ -1,16 +1,22 @@
 class Quarters {
-  public static void main(String[] args) {
-    System.out.println(countQuarters(8734));
-    System.out.println(countQuarters(6824));
-    System.out.println(countQuarters(3670));
-  } 
-  public static int countQuarters(int cents){
-    String cent = Integer.toString(cents);
-    if (2 > cent.length()) {
-      return 0;
+    public static void main(String[] args) {
+        System.out.println(countQuarters(8734));
+        System.out.println(countQuarters(6824));
+        System.out.println(countQuarters(3670));
+    } 
+    //tests
+    public static int countQuarters(int cents){
+        String cent = Integer.toString(cents);
+        //convert to string
+        if (2 > cent.length()) {
+            return 0;
+        }
+        //single digit no quarter value
+        else {
+            return (Integer.valueOf(cent.substring(cent.length() - 2))/25);
+        }
+        //find last two digits, get int value, divide by 25
     }
-    else {
-        return (Integer.valueOf(cent.substring(cent.length() - 2))/25);
-    }
-  }
+    //end countQuarters
 }
+//end class
