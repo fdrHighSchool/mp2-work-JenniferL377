7419128e4880cb52dcdfad5f12028b1b3da84627
JenniferL377 <114942345+JenniferL377@users.noreply.github.com>
2022-12-02 00:42:03 -0500

    done

--- 02-fizzbuzz/FizzBuzz.java
+++ 02-fizzbuzz/FizzBuzz.java
@@ -5,21 +5,28 @@ class FizzBuzz {
             if (i%15 == 0) {
                 System.out.print("\nFizz! Buzz! ");
             }
+	    //fits both
 	    else if (i%3==0) {
 	        System.out.print("\nFizz! ");
 		if (i/3==3) {
 		    System.out.print("Fizz! ");
 		}
             }
+	    //fizz numbers
             else if (i%5==0) {
                 System.out.print("\nBuzz! ");
                 if (i/5==5) {
 		    System.out.print("Buzz! ");
 		}
             }
+	    //buzz numbers
             else {
                 System.out.print("\n"+i);
             }
+	    //numbers that dont fit
         }
+	//range of numbers
     }
+    //end main
 }
+//end class
