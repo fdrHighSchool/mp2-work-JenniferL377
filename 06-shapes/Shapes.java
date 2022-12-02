4b1ef892ad7e85fe57da8052b7f171dd0dccf878
JenniferL377 <114942345+JenniferL377@users.noreply.github.com>
2022-12-01 22:31:59 -0500

    Create Shapes.java

---
+++ 06-shapes/Shapes.java
@@ -0,0 +1,41 @@
+class Shapes {
+    public static void main(String[] args) {
+        System.out.println("Shapes");
+        triangle(8);
+    }
+    public static void square(int x) {
+        for(int row = 1; row <= x; row++) {
+            System.out.println("");
+            for (int col = 1; col <= x; col++) {
+            System.out.print("* ");
+            }
+        }
+    }
+    public static void rectangle(int x, int y) {
+        for(int row = 1; row <= x; row++) {
+            System.out.println("");
+            for (int col = 1; col <= y; col++) {
+            System.out.print("* ");
+            }
+        }
+    }
+    public static void righttriangle(int x) {
+        for(int row = 1; row <= x; row++) {
+            System.out.println("");
+            for (int col = 1; col <= row; col++) {
+            System.out.print("* ");
+            }
+        }
+    }
+    public static void triangle(int x) {
+        for(int row = 1; row <= x; row++) {
+            System.out.println("");
+            for (int space = 1; space <= x-row; space++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++) {
+            System.out.print("* ");
+            }
+        }
+    }
+}
