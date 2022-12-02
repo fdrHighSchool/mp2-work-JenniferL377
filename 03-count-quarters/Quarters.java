398241920f020d619e7e66bba51b4d7a63f4c087
JenniferL377 <114942345+JenniferL377@users.noreply.github.com>
2022-12-02 00:32:22 -0500

    Update Quarters.java

--- 03-count-quarters/Quarters.java
+++ 03-count-quarters/Quarters.java
@@ -1,10 +1,16 @@
-public class Quarters {
-  public static void main(String args[]) {
-    countQuarters(1075);
-  }
-  public static int countQuarters(int cents) {
+class Quarters {
+  public static void main(String[] args) {
+    System.out.println(countQuarters(8734));
+    System.out.println(countQuarters(6824));
+    System.out.println(countQuarters(3670));
+  } 
+  public static int countQuarters(int cents){
     String cent = Integer.toString(cents);
-    Integer.parseInt((cent).length-2);
-    return cent/25;
+    if (2 > cent.length()) {
+      return 0;
+    }
+    else {
+        return (Integer.valueOf(cent.substring(cent.length() - 2))/25);
+    }
   }
 }
