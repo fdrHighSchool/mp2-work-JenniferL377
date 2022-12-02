c9ef24ef792e4d60bc16ebce83b76a3b20977bd7
JenniferL377 <114942345+JenniferL377@users.noreply.github.com>
2022-12-02 00:39:00 -0500

    done

--- 01-leap-year/LeapYear.java
+++ 01-leap-year/LeapYear.java
@@ -4,8 +4,8 @@ class LeapYear {
         System.out.println(isLeapYear(2020));
         System.out.println(isLeapYear(2000));
         System.out.println(isLeapYear(1900));
-        
     }
+    //end main
     public static boolean isLeapYear(int x) {
         if ((x%4) == 0) {
             if ((x%100) == 0) {
@@ -15,13 +15,18 @@ class LeapYear {
                 else {
                     return false;
                 }
+                //t or f if 400
             }
             else {
                 return true;
             }
+            //t or f if 100
         }
         else {
             return false;
         }
+        //t or f if 4
     }
+    //end isLeapYear
 }
+//end class
